3844ce94f0c034afc78059c0c86e10580f4d8c29
hafsaajabboune-hgb <hafsaajabboune@gmail.com>
2026-03-20 01:14:57 +0100

    Update ProduitAvecHorodatage.js

--- src/ProduitAvecHorodatage.js
+++ src/ProduitAvecHorodatage.js
@@ -1,17 +1,9 @@
-// Français : Version avec horodatage du composant CarteProduit
-// English: Version with timestamp of the CarteProduit component
-// Español: Versión con marca de tiempo del componente CarteProduit
-// Deutsch: Version mit Zeitstempel der CarteProduit-Komponente
-// Italiano: Versione con timestamp del componente CarteProduit
+// French/English: Produit avec horodatage / Product with timestamp
 
 import avecHorodatage from './avecHorodatage';
 import CarteProduit from './CarteProduit';
 
-// Français : Application du HOC avecHorodatage au composant CarteProduit
-// English: Application of the avecHorodatage HOC to the CarteProduit component
-// Español: Aplicación del HOC avecHorodatage al componente CarteProduit
-// Deutsch: Anwendung des avecHorodatage HOC auf die CarteProduit-Komponente
-// Italiano: Applicazione dell'HOC avecHorodatage al componente CarteProduit
+// FR: Application du HOC d'horodatage / EN: Applying timestamp HOC
 const ProduitAvecHorodatage = avecHorodatage(CarteProduit);
 
 export default ProduitAvecHorodatage;
