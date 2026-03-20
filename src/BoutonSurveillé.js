fbd6e7b760745d804522da265624199ef7455f61
hafsaajabboune-hgb <hafsaajabboune@gmail.com>
2026-03-20 01:11:32 +0100

    Update BoutonSurveillé.js

--- src/BoutonSurveillé.js
+++ src/BoutonSurveillé.js
@@ -1,17 +1,9 @@
-// Français : Version surveillée du composant BoutonAction
-// English: Monitored version of the BoutonAction component
-// Español: Versión monitoreada del componente BoutonAction
-// Deutsch: Überwachte Version der BoutonAction-Komponente
-// Italiano: Versione monitorata del componente BoutonAction
+// French/English: Version surveillée du bouton / Monitored button version
 
 import avecSurveillance from './avecSurveillance';
 import BoutonAction from './BoutonAction';
 
-// Français : Application du HOC pour créer une version enrichie
-// English: Applying the HOC to create an enhanced version
-// Español: Aplicación del HOC para crear una versión enriquecida
-// Deutsch: Anwendung des HOC zur Erstellung einer erweiterten Version
-// Italiano: Applicazione dell'HOC per creare una versione arricchita
+// FR: Application du HOC / EN: Applying the HOC
 const BoutonSurveillé = avecSurveillance(BoutonAction);
 
 export default BoutonSurveillé;
